5d374934f73fc2aa8a55062f8d878a5c31ef1be5
dzikrikhasnudin <dzikri.khasnudin2@gmail.com>
2021-02-03 20:21:38 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>hellow world</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti dolorem labore perspiciatis sed alias recusandae animi fugit a reiciendis atque necessitatibus iste ex laudantium placeat, illum, enim aliquid laboriosam! Neque!</p>
 </body>
 
 </html>
